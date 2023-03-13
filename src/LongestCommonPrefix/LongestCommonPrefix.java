16dcca2fdd914370adc443084b1122c73b5acba2
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-13 17:40:03 +1100

    longest prefix common

--- src/LongestCommonPrefix/LongestCommonPrefix.java
+++ src/LongestCommonPrefix/LongestCommonPrefix.java
@@ -3,7 +3,20 @@ package LongestCommonPrefix;
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
+		String[] str = { "flower", "flow", "flight" };
+		System.out.println(longestCommonPrefix(str));
+	}
 
+	public static String longestCommonPrefix(String[] strs) {
+		String result = "";
+
+		for (int i = 0; i < strs.length; i++) {
+			String temp = strs[i];
+			char tempChar = strs[i].charAt(i);
+			System.out.println(temp);
+			System.out.println(tempChar);
+		}
+		return result;
 	}
 
 }
