abb6775539b4318a5dd3428af21928df9bfd3900
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-13 18:46:25 +1100

    needed to see chatGpt

--- src/LongestCommonPrefix/LongestCommonPrefix.java
+++ src/LongestCommonPrefix/LongestCommonPrefix.java
@@ -3,20 +3,47 @@ package LongestCommonPrefix;
 public class LongestCommonPrefix {
 
 	public static void main(String[] args) {
-		String[] str = { "flower", "flow", "flight" };
+		String[] str = { "flooad", "flood", "flool" };
 		System.out.println(longestCommonPrefix(str));
+
+		String name = "Ajay";
+		System.out.println(name.indexOf("y"));
 	}
 
 	public static String longestCommonPrefix(String[] strs) {
-		String result = "";
 
-		for (int i = 0; i < strs.length; i++) {
-			String temp = strs[i];
-			char tempChar = strs[i].charAt(i);
-			System.out.println(temp);
-			System.out.println(tempChar);
+//		if (strs == null || strs.length == 0) {
+//			return "";
+//		}
+//		String temp = strs[0];
+//		for (int i = 1; i < strs.length; i++) {
+//
+//			System.out.println(strs[i].indexOf(temp));
+//			while (strs[i].indexOf(temp) != 0) {
+//				temp = temp.substring(0, temp.length() - 1);
+//				if (temp.isEmpty()) {
+//					return "";
+//				}
+//			}
+//		}
+//		return temp;
+
+		int l = 0;
+		int r = 0;
+		String s = strs[0];
+		for (int i = 1; i < strs.length; i++) {
+			for (int j = 0; j < strs[i].length(); j++) {
+				if (j > s.length() || s.length() == j) {
+					break;
+				}
+				if (s.charAt(j) == strs[i].charAt(j)) {
+					continue;
+				} else {
+					s = s.substring(0, j);
+				}
+			}
 		}
-		return result;
+		return s;
 	}
 
 }
