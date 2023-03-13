a4dd3a389cd4e61f155e5489d15e601365c635e1
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-13 21:05:55 +1100

    used binary search to insert search

--- src/SearchInsertPosition/SearchInsertPosition.java
+++ src/SearchInsertPosition/SearchInsertPosition.java
@@ -5,7 +5,7 @@ public class SearchInsertPosition {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] nums = { 1, 3, 5, 6 };
-		System.out.println(searchInsert(nums, 5));
+		System.out.println(searchInsert(nums, 7));
 	}
 
 	/*
@@ -17,25 +17,41 @@ public class SearchInsertPosition {
 	 * 
 	 */
 	public static int searchInsert(int[] nums, int target) {
-		int result = 0;
-		if (nums.length == 1) {
-			if (nums[nums.length - 1] == target && nums[nums.length - 1] > target) {
-				result = nums.length - 1;
-			} else if (nums[nums.length - 1] < target) {
-				result = nums.length;
+//		int result = 0;
+//		if (nums.length == 1) {
+//			if (nums[nums.length - 1] == target && nums[nums.length - 1] > target) {
+//				result = nums.length - 1;
+//			} else if (nums[nums.length - 1] < target) {
+//				result = nums.length;
+//			}
+//
+//		}
+//		for (int i = 0; i < nums.length - 1; i++) {
+//			if (nums[i] < target && nums[i + 1] >= target) {
+//				result = i + 1;
+//			}
+//
+//			if (target > nums[nums.length - 1]) {
+//				result = nums.length;
+//			}
+//		}
+//		return result;
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = (high + low) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else {
+				if (nums[mid] < target) {
+					low = mid + 1;
+				} else {
+					high = mid - 1;
+				}
 			}
-
 		}
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i] < target && nums[i + 1] >= target) {
-				result = i + 1;
-			}
+		return low;
 
-			if (target > nums[nums.length - 1]) {
-				result = nums.length;
-			}
-		}
-		return result;
 	}
 
 }
