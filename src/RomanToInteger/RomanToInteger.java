860837ebca7a5ac2b1d8aa188a3b918256e83374
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-13 16:15:51 +1100

    int to roman wtfff

--- src/RomanToInteger/RomanToInteger.java
+++ src/RomanToInteger/RomanToInteger.java
@@ -3,12 +3,62 @@ package RomanToInteger;
 public class RomanToInteger {
 
 	public static void main(String[] args) {
-
+		System.out.println(romanToInt("I"));
 	}
 
-	public int romanToInt(String s) {
+	public static int romanToInt(String s) {
+		int result = 0;
 
-		return 1;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'I') {
+				if (s.charAt(i + 1) == 'V' || s.charAt(i + 1) == 'X') {
+					result -= 1;
+					i++;
+				} else {
+					result += 1;
+				}
+			}
+
+			if (s.charAt(i) == 'V') {
+				result += 5;
+			}
+
+			if (s.charAt(i) == 'X') {
+				if (s.charAt(i + 1) == 'L' || s.charAt(i + 1) == 'C') {
+					result -= 10;
+					i++;
+
+				} else {
+					result += 10;
+				}
+
+			}
+
+			if (s.charAt(i) == 'L') {
+				result += 50;
+			}
+
+			if (s.charAt(i) == 'C') {
+				if (s.charAt(i + 1) == 'D' || s.charAt(i + 1) == 'M') {
+					result -= 100;
+					i++;
+
+				} else {
+					result += 100;
+				}
+
+			}
+
+			if (s.charAt(i) == 'D') {
+				result += 500;
+			}
+
+			if (s.charAt(i) == 'M') {
+				result += 1000;
+			}
+
+		}
+		return result;
 	}
 
 }
