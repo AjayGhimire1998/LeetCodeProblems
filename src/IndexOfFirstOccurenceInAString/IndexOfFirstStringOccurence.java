0752d7d730323d46de76020eedb66269adc61e71
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-15 23:37:30 +1100

    added question

--- src/IndexOfFirstOccurenceInAString/IndexOfFirstStringOccurence.java
+++ src/IndexOfFirstOccurenceInAString/IndexOfFirstStringOccurence.java
@@ -7,6 +7,10 @@ public class IndexOfFirstStringOccurence {
 		System.out.println(strStr("abc", "c"));
 	}
 
+	/*
+	 * Given two strings needle and haystack, return the index of the first
+	 * occurrence of needle in haystack, or -1 if needle is not part of haystack.
+	 */
 	public static int strStr(String haystack, String needle) {
 //		if (haystack.length() < needle.length()) {
 //			return -1;
