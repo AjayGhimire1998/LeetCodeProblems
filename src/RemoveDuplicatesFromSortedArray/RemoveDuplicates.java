4ce7c04f1b0a6a59150b2d67898f83d06ff7de3f
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-15 21:48:03 +1100

    removed duplicates from an sorted array

--- src/RemoveDuplicatesFromSortedArray/RemoveDuplicates.java
+++ src/RemoveDuplicatesFromSortedArray/RemoveDuplicates.java
@@ -3,7 +3,26 @@ package RemoveDuplicatesFromSortedArray;
 public class RemoveDuplicates {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+		System.out.println(removeDuplicates(nums));
+
+	}
+
+	public static int removeDuplicates(int[] nums) {
+		int k = 1;
+
+		if (nums.length == 0) {
+			return 0;
+		}
+
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i + 1] > nums[i]) {
+				nums[k] = nums[i + 1];
+				k++;
+			}
+		}
+		return k;
 
 	}
 
