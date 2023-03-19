67ab0bee34c3ad592ba201e1599b182898155d70
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-19 13:50:53 +1100

    my solution worked int he the computer

--- src/PlusOne/PlusOne.java
+++ src/PlusOne/PlusOne.java
@@ -1,5 +1,6 @@
 package PlusOne;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 /*
@@ -14,27 +15,37 @@ import java.util.Arrays;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] digits = { 9 };
+		int[] digits = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
 		System.out.println(Arrays.toString(plusOne(digits)));
+
+//		System.out.println(plusOne(digits));
 	}
 
-	public static char[] plusOne(int[] digits) {
-		char[] result;
-		if (digits[digits.length - 1] == 9) {
-			result = new char[digits.length + 1];
-		} else {
-			result = new char[digits.length];
-		}
-		String num = "";
-		int sum = 0;
+	/*
+	 * You are given a large integer represented as an integer array digits, where
+	 * each digits[i] is the ith digit of the integer. The digits are ordered from
+	 * most significant to least significant in left-to-right order. The large
+	 * integer does not contain any leading 0's.
+	 * 
+	 * Increment the large integer by one and return the resulting array of digits.
+	 */
+	public static int[] plusOne(int[] digits) {
+		String str = "";
+
 		for (int i = 0; i < digits.length; i++) {
-			num += Integer.toString(digits[i]);
+			str += Integer.toString(digits[i]);
 		}
-		sum = Integer.parseInt(num) + 1;
-		for (int i = 0; i < Integer.toString(sum).length(); i++) {
-			result[i] = Integer.toString(sum).charAt(i);
+
+//		long resultInt = Long.valueOf(str) + 1;
+		BigInteger resultIntt = new BigInteger(str);
+		resultIntt = resultIntt.add(BigInteger.ONE);
+
+		str = resultIntt.toString();
+		int[] result = new int[str.length()];
+		for (int j = 0; j < str.length(); j++) {
+
+			result[j] = Character.getNumericValue(str.charAt(j));
 		}
-		System.out.println(result);
 		return result;
 	}
 
