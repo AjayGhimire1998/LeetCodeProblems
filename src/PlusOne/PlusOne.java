947909b123b2b4b7cf838d63f89eca8b33773340
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-19 13:58:21 +1100

    i get it now

--- src/PlusOne/PlusOne.java
+++ src/PlusOne/PlusOne.java
@@ -1,6 +1,5 @@
 package PlusOne;
 
-import java.math.BigInteger;
 import java.util.Arrays;
 
 /*
@@ -15,7 +14,7 @@ import java.util.Arrays;
 public class PlusOne {
 
 	public static void main(String[] args) {
-		int[] digits = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+		int[] digits = { 9, 8, 7, 6, 5, 4, 3, 2, 1, 9 };
 		System.out.println(Arrays.toString(plusOne(digits)));
 
 //		System.out.println(plusOne(digits));
@@ -30,23 +29,34 @@ public class PlusOne {
 	 * Increment the large integer by one and return the resulting array of digits.
 	 */
 	public static int[] plusOne(int[] digits) {
-		String str = "";
+//		String str = "";
+//
+//		for (int i = 0; i < digits.length; i++) {
+//			str += Integer.toString(digits[i]);
+//		}
+//
+////		long resultInt = Long.valueOf(str) + 1;
+//		BigInteger resultIntt = new BigInteger(str);
+//		resultIntt = resultIntt.add(BigInteger.ONE);
+//
+//		str = resultIntt.toString();
+//		int[] result = new int[str.length()];
+//		for (int j = 0; j < str.length(); j++) {
+//
+//			result[j] = Character.getNumericValue(str.charAt(j));
+//		}
+//		return result;
 
-		for (int i = 0; i < digits.length; i++) {
-			str += Integer.toString(digits[i]);
+		for (int i = digits.length - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
 		}
-
-//		long resultInt = Long.valueOf(str) + 1;
-		BigInteger resultIntt = new BigInteger(str);
-		resultIntt = resultIntt.add(BigInteger.ONE);
-
-		str = resultIntt.toString();
-		int[] result = new int[str.length()];
-		for (int j = 0; j < str.length(); j++) {
-
-			result[j] = Character.getNumericValue(str.charAt(j));
-		}
-		return result;
+		digits = new int[digits.length + 1];
+		digits[0] = 1;
+		return digits;
 	}
 
 }
