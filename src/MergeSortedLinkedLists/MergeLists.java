b797d9d90d3efd62615b89dcdd685ec6a861429a
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-10 20:36:49 +1000

    whatttt

--- src/MergeSortedLinkedLists/MergeLists.java
+++ src/MergeSortedLinkedLists/MergeLists.java
@@ -1,5 +1,29 @@
 package MergeSortedLinkedLists;
 
 public class MergeLists {
+	private static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
 
 }
