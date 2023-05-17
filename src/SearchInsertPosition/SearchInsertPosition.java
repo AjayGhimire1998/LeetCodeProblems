f7c110c487eadb27044dc5af288c124955557741
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-17 20:33:45 +1000

    search insert position

--- src/SearchInsertPosition/SearchInsertPosition.java
+++ src/SearchInsertPosition/SearchInsertPosition.java
@@ -17,41 +17,61 @@ public class SearchInsertPosition {
 	 * 
 	 */
 	public static int searchInsert(int[] nums, int target) {
-		int result = 0;
-		if (nums.length == 1) {
-			if (nums[nums.length - 1] == target && nums[nums.length - 1] > target) {
-				result = nums.length - 1;
-			} else if (nums[nums.length - 1] < target) {
-				result = nums.length;
-			}
+//		int result = 0;
+//		if (nums.length == 1) {
+//			if (nums[nums.length - 1] == target && nums[nums.length - 1] > target) {
+//				result = nums.length - 1;
+//			} else if (nums[nums.length - 1] < target) {
+//				result = nums.length;
+//			}
+//
+//		}
+//		for (int i = 0; i < nums.length - 1; i++) {
+//			if (nums[i] < target && nums[i + 1] >= target) {
+//				result = i + 1;
+//			}
+//
+//			if (target > nums[nums.length - 1]) {
+//				result = nums.length;
+//			}
+//		}
+//		return result;
 
-		}
-		for (int i = 0; i < nums.length - 1; i++) {
-			if (nums[i] < target && nums[i + 1] >= target) {
-				result = i + 1;
-			}
+//		int low = 0;
+//		int high = nums.length - 1;
+//		while (low <= high) {
+//			int mid = (high + low) / 2;
+//			if (nums[mid] == target) {
+//				return mid;
+//			} else {
+//				if (nums[mid] < target) {
+//					low = mid + 1;
+//				} else {
+//					high = mid - 1;
+//				}
+//			}
+//		}
+//		return low;
 
-			if (target > nums[nums.length - 1]) {
-				result = nums.length;
-			}
-		}
-		return result;
 		int low = 0;
 		int high = nums.length - 1;
+
 		while (low <= high) {
 			int mid = (high + low) / 2;
+
 			if (nums[mid] == target) {
 				return mid;
 			} else {
 				if (nums[mid] < target) {
+
 					low = mid + 1;
 				} else {
 					high = mid - 1;
 				}
 			}
 		}
-		return low;
 
+		return low;
 	}
 
 }
