78f14c391be94a06f42cf45c91759aac25a1524f
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-17 20:11:12 +1000

    pte passed

--- src/SearchInsertPosition/SearchInsertPosition.java
+++ src/SearchInsertPosition/SearchInsertPosition.java
@@ -36,21 +36,21 @@ public class SearchInsertPosition {
 			}
 		}
 		return result;
-//		int low = 0;
-//		int high = nums.length - 1;
-//		while (low <= high) {
-//			int mid = (high + low) / 2;
-//			if (nums[mid] == target) {
-//				return mid;
-//			} else {
-//				if (nums[mid] < target) {
-//					low = mid + 1;
-//				} else {
-//					high = mid - 1;
-//				}
-//			}
-//		}
-//		return low;
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = (high + low) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else {
+				if (nums[mid] < target) {
+					low = mid + 1;
+				} else {
+					high = mid - 1;
+				}
+			}
+		}
+		return low;
 
 	}
 
