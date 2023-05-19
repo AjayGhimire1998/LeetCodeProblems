f56a5460189c5f9cd5a2390f266b37dd6f9be821
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-19 18:13:06 +1000

    printer

--- src/githubPrac/newTest.java
+++ src/githubPrac/newTest.java
@@ -4,4 +4,8 @@ public class newTest {
 	public static void main(String[] args) {
 		System.out.println("new test testedx");
 	}
+
+	public void printer() {
+		System.out.println("printinggggg moneyyy");
+	}
 }
